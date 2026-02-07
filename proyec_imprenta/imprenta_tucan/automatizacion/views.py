037f8c3e1b62957c7ba69aa4162061718860f531
Lopez Luciano <3luciano10@gmail.com>
2026-02-07 10:19:58 -0300

    Se reorganizo el dashboard

--- proyec_imprenta/imprenta_tucan/automatizacion/views.py
+++ proyec_imprenta/imprenta_tucan/automatizacion/views.py
@@ -3,6 +3,8 @@ from django.core.paginator import Paginator
 from django.http import JsonResponse
 from .models import OrdenSugerida, OfertaAutomatica
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 
 
 def panel(request):
@@ -197,3 +199,6 @@ def _ensure_demo_ordenes():
             insumo=insumo,
             cantidad=cantidades[idx % len(cantidades)]
         )
+
+
+# Logs de Automatización removidos del panel y rutas
