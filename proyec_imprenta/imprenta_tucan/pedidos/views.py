8582f77961b9cdd4289be0a6e700930ff10432ca
Lopez Luciano <3luciano10@gmail.com>
2026-04-05 19:47:48 -0300

    Correcciones en Crear presupuesstos

--- proyec_imprenta/imprenta_tucan/pedidos/views.py
+++ proyec_imprenta/imprenta_tucan/pedidos/views.py
@@ -1,6 +1,6 @@
 from permisos.decorators import requiere_permiso
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.utils import timezone
 from django.core.paginator import Paginator
